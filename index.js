1994d692224d8409c04c5acf9faea90edb3ed091
Volodymyr Smoliak <svovka83@gmail.com>
2026-01-23 05:45:19 +0200

    refactoring

--- index.js
+++ index.js
@@ -137,6 +137,7 @@ kindOfBtn3.addEventListener("click", () => {
 		}
 	}
 });
+
 const kindOfBtn4 = document.querySelector(".footer-list.list-group__item:nth-child(4)>h3");
 const dropDownList4 = document.querySelector(".footer-list.list-group__item:nth-child(4)>.footer-list__items");
 const dropDownIcon4 = document.querySelector(".list-group__item:nth-child(4)");
@@ -152,6 +153,7 @@ kindOfBtn4.addEventListener("click", () => {
 		}
 	}
 });
+
 const kindOfBtn5 = document.querySelector(".footer-list.list-group__item:nth-child(5)>h3");
 const dropDownList5 = document.querySelector(".footer-list.list-group__item:nth-child(5)>.footer-list__items");
 const dropDownIcon5 = document.querySelector(".list-group__item:nth-child(5)");
@@ -167,3 +169,120 @@ kindOfBtn5.addEventListener("click", () => {
 		}
 	}
 });
+
+// activing explore collections' buttons
+
+const body = document.body;
+const exploreBtn1 = document.querySelector(".collection-list__item:nth-child(1)>a");
+const exploreBtn2 = document.querySelector(".collection-list__item:nth-child(2)>a");
+const exploreBtn3 = document.querySelector(".collection-list__item:nth-child(3)>a");
+const exploreBtn4 = document.querySelector(".collection-list__item:nth-child(4)>a");
+const exploreBtn5 = document.querySelector(".collection-list__item:nth-child(5)>a");
+const exploreBtn6 = document.querySelector(".collection-list__item:nth-child(6)>a");
+const exploreBtn7 = document.querySelector(".collection-list__item:nth-child(7)>a");
+const exploreBtn8 = document.querySelector(".collection-list__item:nth-child(8)>a");
+
+const exploreButtons = new Array(
+	exploreBtn1,
+	exploreBtn2,
+	exploreBtn3,
+	exploreBtn4,
+	exploreBtn5,
+	exploreBtn6,
+	exploreBtn7,
+	exploreBtn8
+);
+
+console.log(exploreButtons.length);
+
+exploreBtn1.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn1.classList.add("collection-list__link--active");
+});
+exploreBtn2.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn2.classList.add("collection-list__link--active");
+});
+exploreBtn3.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn3.classList.add("collection-list__link--active");
+});
+exploreBtn4.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn4.classList.add("collection-list__link--active");
+});
+exploreBtn5.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn5.classList.add("collection-list__link--active");
+});
+exploreBtn6.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn6.classList.add("collection-list__link--active");
+});
+exploreBtn7.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn7.classList.add("collection-list__link--active");
+});
+exploreBtn8.addEventListener("click", () => {
+	exploreButtons.forEach((el) => {
+		el.classList.remove("collection-list__link--active");
+	});
+	exploreBtn8.classList.add("collection-list__link--active");
+});
+
+// Happy Christmas colorful background blur circles implementation
+
+function positionAmazingCircle(startTopPositionCircle, startleftPositionCircle, blurClass) {
+	let correntTopIncrement = 0;
+	let correntLeftIncrement = 0;
+
+	for (let i = 0; i < 9; i++) {
+		const circle = document.createElement("span");
+		circle.classList.add(blurClass);
+
+		const correntTopIndent = startTopPositionCircle + correntTopIncrement;
+		if (correntTopIndent > 85) {
+			startTopPositionCircle = 5;
+			correntTopIncrement = -10;
+		}
+
+		const correntLeftIndent = startleftPositionCircle + correntLeftIncrement;
+		if (correntLeftIndent > 78) {
+			startleftPositionCircle = 5;
+			correntLeftIncrement = -10;
+		}
+
+		circle.style.top = `${correntTopIndent}%`;
+		circle.style.left = `${correntLeftIndent}%`;
+
+		document.body.appendChild(circle);
+
+		correntTopIncrement += 14;
+		correntLeftIncrement += 14;
+	}
+}
+
+positionAmazingCircle(1, 5, "blur-circle-pink");
+positionAmazingCircle(6, 15, "blur-circle-pink");
+positionAmazingCircle(2, 25, "blur-circle-rose");
+positionAmazingCircle(7, 35, "blur-circle-pink");
+positionAmazingCircle(3, 45, "blur-circle-pink");
+positionAmazingCircle(8, 55, "blur-circle-blue");
+positionAmazingCircle(4, 65, "blur-circle-pink");
+positionAmazingCircle(9, 75, "blur-circle-rose");
+positionAmazingCircle(5, 85, "blur-circle-pink");
+positionAmazingCircle(0, 95, "blur-circle-pink");
